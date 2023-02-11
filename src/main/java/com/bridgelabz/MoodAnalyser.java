8f9d72b10ae0b4678ba91134444670046919da51
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-11 08:08:42 +0530

    Added: Analysing the Mood Between Sad or Happy

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,4 +2,15 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
 
+    public String analyseMood(String message) {
+        if (message.contains("Sad")){
+            return "SAD";
+        }
+        else if (message.contains("Happy")) {
+            return "HAPPY";
+        }
+        else {
+            return "Unknown";
+        }
+    }
 }
