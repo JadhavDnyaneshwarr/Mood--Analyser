865c0a0629c59396a30e25f3237c238a3b9a1f8a
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-11 21:16:06 +0530

    Refactor: using Parametrized constructor to give message

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -3,18 +3,21 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class MoodAnalyserTest {
 
-    static MoodAnalyser moodAnalyser = new MoodAnalyser();
     @Test
     public void whenGivenMoodShouldReturnSAD(){
-        String mood = moodAnalyser.analyseMood("i am in Sad Mood");
-        Assertions.assertEquals("SAD", mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("i am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertEquals("SAD", mood);
     }
     @Test
     public void whenGivenAnyMoodShouldReturnHAPPY(){
-        String mood = moodAnalyser.analyseMood("i am in Any Mood");
-        Assertions.assertEquals("HAPPY", mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("i am in Any Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertEquals("HAPPY", mood);
     }
 }
